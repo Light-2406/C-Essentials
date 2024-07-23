1d36ff3d378582d9920ac9d6107a32fd05a5b7b5
Light <153726173+Light-2406@users.noreply.github.com>
2024-07-23 10:29:11 +0530

    Create Temperature Classifier.C

---
+++ Temperature Classifier.C
@@ -0,0 +1,31 @@
+// Problem Statement : Considering Mangalore/Udupi's Climate entire known as Coastal Karnataka, here's a code in tulu i.e. regional language of them.
+// Note : This covers your temperature in celsius
+// Program :
+// Problem Statement : Considering Mangalore/Udupi's Climate entire known as Coastal Karnataka, here's a code in tulu i.e. regional language of them.
+// Note : This covers your temperature in celsius
+// Program :
+#include <stdio.h>
+int main ()
+{
+float temp;
+printf("Temperature yet undu,celsius d padu: ");
+scanf("%f",&temp);
+if (temp>=20 && temp <=26)
+{
+    printf ("Kode pasond pola ! Onja barsa ipu ijinda dombu ipu !!");
+}
+else if (temp >= 27 && temp <= 34)
+{
+    printf("Illad kulu mare ya jor dombu und !!");
+}
+else 
+{
+    printf("Saithaaaaa!!!, Nana devere gatti nik");
+}
+return 0;
+}
+
+
+
+
+
